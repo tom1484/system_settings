9ab79b078a852db086d7c574b2d08ec230c74dbb
tom1484 <angelus0918866416@gmail.com>
2020-08-20 12:13:00 +0800

    update

--- .sudhindra.zsh
+++
@@ -1,6 +0,0 @@
-###################################[ custom_os_icon: customized os icon ]######################
-# os icon and user
-POWERLEVEL9K_CUSTOM_OS_ICON='echo  $(whoami)'
-# os icon color
-POWERLEVEL9K_CUSTOM_OS_ICON_BACKGROUND='blue'
-POWERLEVEL9K_CUSTOM_OS_ICON_FOREGROUND='black'
