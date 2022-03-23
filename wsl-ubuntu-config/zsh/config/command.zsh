deb943304f66f3fddbf60771e8d825584ba16ffc
tom1484 <angelus0918866416@gmail.com>
2022-03-23 11:28:20 +0800

    Update

---
+++ wsl-ubuntu-config/zsh/config/command.zsh
@@ -0,0 +1,13 @@
+# push/pull Documents
+alias pd='zsh ~/.push_documents'
+alias pp='zsh ~/.pull_documents'
+
+# easy push
+alias UpdateRepo="~/.zsh/script/update_repo.sh"
+
+alias SetGoPath="source ~/.zsh/script/set_go_path.sh"
+alias CreateGoProject="zsh ~/.zsh/script/create_go_project.sh"
+
+alias jupyter-notebook="~/.local/bin/jupyter-notebook --no-browser"
+alias jupyter-lab="~/.local/bin/jupyter-lab --no-browser"
+
