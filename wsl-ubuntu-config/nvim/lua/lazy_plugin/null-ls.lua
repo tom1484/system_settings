585a7244b9dc96a08ec6efb816e13a533c523f13
tom1484 <angelus0918866416@gmail.com>
2023-02-10 02:35:05 +0800

    Update

--- wsl-ubuntu-config/nvim/lua/lazy_plugin/null-ls.lua
+++ wsl-ubuntu-config/nvim/lua/lazy_plugin/null-ls.lua
@@ -8,45 +8,44 @@ local augroup = vim.api.nvim_create_augroup("lspformatting", {})
 null_ls.setup({
 	border = "rounded",
 	sources = {
-        formatting.autopep8,
-        formatting.astyle.with({
-            filetypes = { "cpp", "c" },
-        }),
-        formatting.beautysh,
-        formatting.cbfmt,
-        formatting.eslint_d,
-        formatting.gofumpt,
-        formatting.goimports,
-        formatting.prettierd.with({
-            filetypes = { "html", "css", "json" },
-        }),
-        formatting.rustfmt,
-        formatting.shfmt,
+		formatting.clang_format.with({
+			extra_args = {
+				"--style", "{BasedOnStyle: Google, IndentWidth: 4, ColumnLimit: 100}",
+			},
+		}),
+		formatting.autopep8,
+		formatting.beautysh,
+		formatting.cbfmt,
+		formatting.eslint_d,
+		formatting.gofumpt,
+		formatting.goimports,
+		formatting.prettierd.with({
+			filetypes = { "html", "css", "json" },
+		}),
+		formatting.rustfmt,
+		formatting.shfmt,
 		formatting.stylua,
+		formatting.jq,
 
-		-- diagnostics.eslint_d.with({
-		-- 	extra_args = { "--tab-width", 2 },
-		-- }),
-		-- diagnostics.cpplint,
 		diagnostics.commitlint,
 		diagnostics.jsonlint,
 		diagnostics.staticcheck,
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			-- vim.keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()")
-			vim.keymap.set("n", "<leader>vf", function()
-				vim.lsp.buf.format()
-				vim.cmd.w()
-			end)
-			-- vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			-- vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 	group = augroup,
-			-- 	buffer = bufnr,
-			-- 	callback = function()
-			-- 		vim.lsp.buf.format({ bufnr = bufnr })
-			-- 	end,
-			-- })
-		end
-	end,
+	-- on_attach = function(client, bufnr)
+	-- 	if client.supports_method("textDocument/formatting") then
+	-- 		vim.keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()")
+	-- 		vim.keymap.set("n", "<leader>vf", function()
+	-- 			vim.lsp.buf.format()
+	-- 			vim.cmd.w()
+	-- 		end)
+	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 			group = augroup,
+	-- 			buffer = bufnr,
+	-- 			callback = function()
+	-- 				vim.lsp.buf.format({ bufnr = bufnr })
+	-- 			end,
+	-- 		})
+	-- 	end
+	-- end,
 })
